4af08587582e2867e6687efc7f64c81f75f57540
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-19 13:15:07 -0400

    ok

---
+++ src/components/header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+      {/* ============ Image Start here ================ */}
+
+      {/* ============ Image End here ================== */}
+      {/* ============ Deliver Start here ============== */}
+
+      {/* ============ Deliver End here ================ */}
+      {/* ============ Search Start here =============== */}
+
+      {/* ============ Search End here ================= */}
+      {/* ============ Signing Start here =============== */}
+
+      {/* ============ Signing End here ================= */}
+      {/* ============ Orders Start here =============== */}
+
+      {/* ============ Orders End here ================= */}
+      {/* ============ Cart Start here ================= */}
+
+      {/* ============ Cart End here =================== */}
+    </div>
+  );
+};
+
+export default Header;
