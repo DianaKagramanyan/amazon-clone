44ed6d9041630ef0cc11fb37669a436d13ab9410
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-24 15:57:46 -0400

    part add to cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import {
   deleteItem,
@@ -7,9 +7,9 @@ import {
   increaseQuantity,
   decreaseQuantity,
 } from "../redux/amazonSlice";
-import { motion } from "framer-motion";
-import { emptyCart } from "../assets";
-import { Link } from "react-router-dom";
+import {motion} from "framer-motion";
+import {emptyCart} from "../assets";
+import {Link} from "react-router-dom";
 
 const Cart = () => {
   const products = useSelector((state) => state.amazonReducer.products);
@@ -31,7 +31,8 @@ const Cart = () => {
         <div className="container mx-auto h-auto grid grid-cols-5 gap-8">
           <div className="w-full bg-white px-4 col-span-5 xl:col-span-4">
             {/* ====================== Cart title Start here ======================= */}
-            <div className="font-titleFont hidden xl:flex items-center justify-between border-b-[1px] border-b-gray-400 py-3">
+            <div
+              className="font-titleFont hidden xl:flex items-center justify-between border-b-[1px] border-b-gray-400 py-3">
               <h1 className="text-3xl font-semibold">Shopping Cart</h1>
               <h3 className="text-xl font-semibold">Subtotal</h3>
             </div>
@@ -59,7 +60,8 @@ const Cart = () => {
                         Unit Price:{" "}
                         <span className="font-semibold">${item.price}</span>
                       </p>
-                      <div className="bg-[#F0F2F2] flex justify-center items-center gap-2 w-36 py-1 text-center drop-shadow-lg rounded-md">
+                      <div
+                        className="bg-[#F0F2F2] flex justify-center items-center gap-2 w-36 py-1 text-center drop-shadow-lg rounded-md">
                         <p className="text-base font-normal">Qty:</p>
                         <p
                           onClick={() => {
@@ -101,7 +103,8 @@ const Cart = () => {
             </div>
             {/* ====================== Products End here =========================== */}
             <div onClick={() => dispatch(resetCart())} className="w-full py-4">
-              <button className="px-10 py-2 bg-red-500 hover:bg-red-600 active:bg-red-500 text-white rounded-lg font-titleFont font-semibold text-lg tracking-wide">
+              <button
+                className="px-10 py-2 bg-red-500 hover:bg-red-600 active:bg-red-500 text-white rounded-lg font-titleFont font-semibold text-lg tracking-wide">
                 Clear Cart
               </button>
             </div>
@@ -110,7 +113,7 @@ const Cart = () => {
             <div>
               <p className="flex gap-2 items-start text-sm">
                 <span>
-                  <CheckCircleIcon className="bg-white text-green-500 rounded-full" />
+                  <CheckCircleIcon className="bg-white text-green-500 rounded-full"/>
                 </span>
                 Your order qualifies for FREE Shipping Choose this option at
                 checkout. See details....
@@ -120,7 +123,8 @@ const Cart = () => {
                   Total: <span className="text-lg font-bold">${totalAmt}</span>
                 </p>
               </div>
-              <button className="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
+              <button
+                className="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
                 Proceed to Buy
               </button>
             </div>
@@ -128,9 +132,9 @@ const Cart = () => {
         </div>
       ) : (
         <motion.div
-          initial={{ y: 70, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.5 }}
+          initial={{y: 70, opacity: 0}}
+          animate={{y: 0, opacity: 1}}
+          transition={{delay: 0.5, duration: 0.5}}
           className="flex justify-center items-center gap-4 py-10"
         >
           <div>
@@ -149,7 +153,8 @@ const Cart = () => {
               books, electronics, videos, etc. and make it happy.
             </p>
             <Link to="/">
-              <button className="mt-6 bg-yellow-400 rounded-md cursor-pointer hover:bg-yellow-500 active:bg-yellow-700 px-8 py-2 font-titleFont font-semibold text-lg">
+              <button
+                className="mt-6 bg-yellow-400 rounded-md cursor-pointer hover:bg-yellow-500 active:bg-yellow-700 px-8 py-2 font-titleFont font-semibold text-lg">
                 Continue Shopping
               </button>
             </Link>
