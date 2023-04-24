c013effb41b694a58e71c7666f8fe7d1ac9a128e
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-24 09:24:02 -0400

    part I done

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -1,12 +1,13 @@
-// import React from 'react'
-// import { useLoaderData } from 'react-router-dom'
-//
-// const Products = () => {
-//   const data = useLoaderData()
-//   const productData = data.data;
-//   return (
-//     <div>Products</div>
-//   )
-// }
-//
-// export default Products
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+const Products = () => {
+  const data = useLoaderData()
+  const productData = data.data;
+  console.log(productData)
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
