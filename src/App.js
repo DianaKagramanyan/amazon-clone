60faaa1f6445d64288d02c6a52ae7168d0e994f8
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-25 10:51:31 -0400

    part IV sign in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/header/Header";
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,31 +8,41 @@ import {
   ScrollRestoration,
 } from "react-router-dom";
 import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import Home from "./pages/Home";
-import {productsData} from "./api/api";
 import Cart from "./pages/Cart";
-
+import Signin from "./pages/Signin";
+// import Registration from "./pages/Registration";
+// import Checkout from "./pages/Checkout";
+import { productsData } from "./api/api";
 
 const Layout = () => {
   return (
     <div>
-      <Header/>
-      <ScrollRestoration/>
-      <Outlet/>
-      <Footer/>
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 function App() {
-  const router =  createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route index element={<Home/>} loader={productsData}></Route>
-      <Route path="/cart" element={<Cart/>}></Route>
-    </Route>
-  ))
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} loader={productsData}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        {/*<Route path="/registration" element={<Registration />}></Route>*/}
+        {/*<Route path="/checkout" element={<Checkout />}></Route>*/}
+      </Route>
+    )
+  );
   return (
-    <div className='font-bodyFont bg-gray-100'>
+    <div className="font-bodyFont bg-gray-100">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
