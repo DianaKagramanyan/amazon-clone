337ed4da2ff6d12f88e57041c13d2ef38cd76cdd
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-26 09:52:16 -0400

    corrected mistake

--- src/components/header/SideNavContent.js
+++ src/components/header/SideNavContent.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
-const SideNavContent = ({title,one,two,three}) => {
+const SideNavContent = ({ title, one, two, three }) => {
   return (
     <div className="py-3 border-b-[1px] border-b-gray-300">
       <h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
         {title}
       </h3>
-      <ul className='text-sm'>
+      <ul className="text-sm">
         <li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
           {one}
           <span>
@@ -29,6 +29,6 @@ const SideNavContent = ({title,one,two,three}) => {
       </ul>
     </div>
   );
-}
+};
 
-export default SideNavContent
+export default SideNavContent;
