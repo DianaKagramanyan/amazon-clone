a5ebde8391fe44d434c3486f6acd8202e79ecc98
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-04-27 10:15:34 -0400

    partV

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import React, {useEffect, useRef, useState} from "react";
+import {useSelector} from "react-redux";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { allItems } from "../../constants";
-import { logo } from "../../assets/index";
+import {allItems} from "../../constants";
+import {logo} from "../../assets/index";
 import HeaderBottom from "./HeaderBottom";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Header = () => {
   const products = useSelector((state) => state.amazonReducer.products);
@@ -27,17 +27,18 @@ const Header = () => {
 
   return (
     <div className="sticky top-0 z-50">
-      <div className="w-full bg-amazon_blue text-white px-4 py-3 flex md:justify-between items-center gap-2 md:gap-4 lgl:gap-2 xl:gap-4">
+      <div
+        className="w-full bg-amazon_blue text-white px-4 py-3 flex md:justify-between items-center gap-2 md:gap-4 lgl:gap-2 xl:gap-4">
         {/* ===================== Header Image Start here ======================== */}
         <Link to="/">
           <div className="headerHover">
-            <img className="w-24 mt-2" src={logo} alt="logoImage" />
+            <img className="w-24 mt-2" src={logo} alt="logoImage"/>
           </div>
         </Link>
         {/* ===================== Header Image End here ========================== */}
         {/* ===================== Header Deliver Start here ====================== */}
         <div className="hidden md:inline-flex headerHover">
-          <LocationOnOutlinedIcon />
+          <LocationOnOutlinedIcon/>
           <p className="flex flex-col text-xs text-lightText font-light">
             Deliver to{" "}
             <span className="text-sm font-semibold -mt-1 text-whiteText">
@@ -54,7 +55,7 @@ const Header = () => {
           >
             All{" "}
             <span>
-              <ArrowDropDownOutlinedIcon />
+              <ArrowDropDownOutlinedIcon/>
             </span>
           </span>
           {showAll && (
@@ -79,23 +80,31 @@ const Header = () => {
             className="h-full text-base text-amazon_blue flex-grow outline-none border-none px-2"
             type="text"
           />
-          <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md">
-            <SearchIcon />
+          <span
+            className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md">
+            <SearchIcon/>
           </span>
         </div>
         {/* ===================== Header Search End here ========================== */}
         {/* ===================== Header Signin Start here ======================== */}
         <Link to="/signin">
           <div className="flex flex-col items-start justify-center headerHover">
-
-            <p className="text-xs text-lightText font-light">
-              Hello, sign in
-            </p>
+            {
+              userInfo ? (
+                <p className="text-sm text-gray-100 font-medium">
+                  {userInfo.userName}
+                </p>
+              ) : (
+                <p className="text-xs text-lightText font-light">
+                  Hello, sign in
+                </p>
+              )
+            }
 
             <p className="hidden md:inline-flex text-sm font-semibold -mt-1 text-whiteText">
               Accounts & Lists{" "}
               <span>
-                <ArrowDropDownOutlinedIcon />
+                <ArrowDropDownOutlinedIcon/>
               </span>
             </p>
           </div>
@@ -110,11 +119,12 @@ const Header = () => {
         {/* ===================== Header Cart Start here ========================== */}
         <Link to="/cart">
           <div className="flex items-start justify-center headerHover relative">
-            <ShoppingCartIcon />
+            <ShoppingCartIcon/>
             <p className="hidden mdl:inline-flex text-xs font-semibold mt-3 text-whiteText">
               Cart
             </p>
-            <span className="absolute text-xs top-0 left-6 w-4 font-semibold p-1 h-4 bg-[#f3a847] text-amazon_blue rounded-full flex justify-center items-center">
+            <span
+              className="absolute text-xs top-0 left-6 w-4 font-semibold p-1 h-4 bg-[#f3a847] text-amazon_blue rounded-full flex justify-center items-center">
               {products.length > 0 ? products.length : 0}
             </span>
           </div>
@@ -124,7 +134,7 @@ const Header = () => {
 
             className="flex flex-col justify-center items-center headerHover relative"
           >
-            <LogoutIcon />
+            <LogoutIcon/>
             <p className="hidden mdl:inline-flex text-xs font-semibold text-whiteText">
               Log out
             </p>
@@ -144,7 +154,7 @@ const Header = () => {
         {/* ============ Cart Start here ================= */}
         {/* ============ Cart End here =================== */}
       </div>
-      <HeaderBottom />
+      <HeaderBottom/>
     </div>
   );
 };
